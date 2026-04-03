7564482baaacecb9d48c2fe848edf70d77c8dcb9
Cid437 <donntorres26@gmail.com>
2026-04-03 19:10:49 +0800

    commands2

--- resources/views/remember.blade.php
+++ resources/views/remember.blade.php
@@ -1,3 +1,30 @@
-<div>
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
-</div>
+@section('body')
+
+{{--
+
+    Create Laravel file
+        composer create-project laravel/laravel projName
+    Create a view
+        php artisan make:view viewName
+    Create a controller
+        php artisan make:controller controllerName
+    Create a model
+        php artisan make:model modelName
+    create a migration
+        php artisan make:migration create_tablename_table
+    run migration
+        php artisan migrate
+    run migration with seeding
+        php artisan migrate --seed
+    run seeding
+        php artisan db:seed
+    run migration with fresh
+        php artisan migrate:fresh
+    run migration with fresh and seeding
+        php artisan migrate:fresh --seed
+    run server
+        php artisan serve    
+
+--}}
+
+@endsection
