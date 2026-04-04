22f72d9b26cf42eb81cc9271cd886632269bd945
Cid437 <donntorres26@gmail.com>
2026-04-04 14:43:13 +0800

    customer controller & models skeleton

--- resources/views/remember.blade.php
+++ resources/views/remember.blade.php
@@ -4,12 +4,16 @@
 
     Create Laravel file
         composer create-project laravel/laravel projName
-    Create a view
-        php artisan make:view viewName
     Create a controller
         php artisan make:controller controllerName
+        php artisan make:controller controllerName --resource
+        php artisan make:controller controllerName --api    
     Create a model
         php artisan make:model modelName
+    Create a view
+        php artisan make:view viewName
+    Create a route
+        php artisan make:route routeName
     create a migration
         php artisan make:migration create_tablename_table
     run migration
